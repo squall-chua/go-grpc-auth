58479f5915f072f1541a9e3f35c45ec21360053f
Squall Chua <squallchua@gmail.com>
2026-05-19 13:29:20 +0800

    feat: generate greeter proto Go code

--- scripts/gen-proto.sh
+++ scripts/gen-proto.sh
@@ -15,6 +15,7 @@ echo "Moving generated Go files to api/v1..."
 mv api/v1/api/proto/admin/v1 api/v1/admin
 mv api/v1/api/proto/auth/v1 api/v1/auth
 mv api/v1/api/proto/options/v1 api/v1/options
+mv api/v1/api/proto/greeter/v1 api/v1/greeter
 rm -rf api/v1/api
 
 echo "Code generation complete!"
