362bb35aeb072e1605be026a56ae575a2be8bac1
Squall Chua <squallchua@gmail.com>
2026-06-04 10:44:41 +0800

    feat(social): add facebook/twitter/apple provider constants and EmailVerified field

--- internal/domain/social.go
+++ internal/domain/social.go
@@ -5,23 +5,28 @@ import "context"
 type SocialProvider string
 
 const (
-	ProviderGoogle SocialProvider = "google"
-	ProviderGitHub SocialProvider = "github"
+	ProviderGoogle   SocialProvider = "google"
+	ProviderGitHub   SocialProvider = "github"
+	ProviderFacebook SocialProvider = "facebook"
+	ProviderTwitter  SocialProvider = "twitter"
+	ProviderApple    SocialProvider = "apple"
 )
 
 type SocialIdentity struct {
-	Provider   SocialProvider `bson:"provider"`
-	ExternalID string         `bson:"external_id"`
-	Email      string         `bson:"email"`
-	Name       string         `bson:"name"`
-	AvatarURL  string         `bson:"avatar_url"`
+	Provider      SocialProvider `bson:"provider"`
+	ExternalID    string         `bson:"external_id"`
+	Email         string         `bson:"email"`
+	Name          string         `bson:"name"`
+	AvatarURL     string         `bson:"avatar_url"`
+	EmailVerified bool           `bson:"email_verified"`
 }
 
 type SocialUser struct {
-	ID        string
-	Email     string
-	Name      string
-	AvatarURL string
+	ID            string
+	Email         string
+	Name          string
+	AvatarURL     string
+	EmailVerified bool
 }
 
 type SocialProviderInterface interface {
